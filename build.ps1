c46f7ff21976a814ee5a323031fe3fca48868366
moonheart <fzp1205561571@hotmail.com>
2022-11-28 23:31:31 +0800

    ci: fix build

--- build.ps1
+++ build.ps1
@@ -2,7 +2,9 @@ param([string]$configuration='Release')
 
 dotnet build
 
-dotnet publish ClashGui/ClashGui.csproj -c $configuration -o ./publish/ -r win-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true
+dotnet publish ClashGui/ClashGui.csproj -c $configuration -o ./publish-win/ -r win-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true
+dotnet publish ClashGui.WindowsService/ClashGui.WindowsService.csproj -c $configuration -o ./publish-win/ -r win-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true
 
-dotnet publish ClashGui.WindowsService/ClashGui.WindowsService.csproj -c $configuration -o ./publish/ -r win-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true
+dotnet publish ClashGui/ClashGui.csproj -c $configuration -o ./publish-linux/ -r linux-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true
+dotnet publish ClashGui.WindowsService/ClashGui.WindowsService.csproj -c $configuration -o ./publish-linux/ -r linux-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true
 
