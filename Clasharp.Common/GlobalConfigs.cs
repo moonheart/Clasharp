1a7011d145ef39fc6dad28c86b6a59f22b29a7c7
moonheart <fzp1205561571@hotmail.com>
2022-12-04 16:54:57 +0800

    feat: Add service status platform specific operation

--- Clasharp.Common/GlobalConfigs.cs
+++ Clasharp.Common/GlobalConfigs.cs
@@ -8,5 +8,6 @@ public static class GlobalConfigs
     public static string MainConfig = Path.Combine(ProgramHome, "main.json");
     public static string ProfilesDir = Path.Combine(ProgramHome, "profiles/");
     public static string ClashExe = Path.Combine(ProgramHome, "Clash.Meta-windows-amd64.exe");
+    public const string CoreServiceName = "clasharp-core-service"; 
     public const int ClashServicePort = 62134;
 }
