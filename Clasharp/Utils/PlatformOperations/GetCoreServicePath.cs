a2dc635ef9c5eb6b935c2f4c5a9f015e4e9811f4
moonheart <fzp1205561571@hotmail.com>
2022-12-06 23:06:31 +0800

    feat: Add PSO GetCoreServicePath

---
+++ Clasharp/Utils/PlatformOperations/GetCoreServicePath.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+
+namespace Clasharp.Utils.PlatformOperations;
+
+public class GetCoreServicePath: PlatformSpecificOperation<string>
+{
+    protected override Task<string> DoForWindows()
+    {
+#if DEBUG
+        return Task.FromResult(@"D:\Git_Github\Clasharp\Clasharp.Service\bin\Debug\net6.0\Clasharp.Service.exe");
+#else
+        return Task.FromResult(System.IO.Path.Combine(System.Environment.CurrentDirectory, "Clasharp.Service.exe"));
+#endif
+    }
+
+    protected override Task<string> DoForLinux()
+    {
+#if DEBUG
+        return Task.FromResult(System.IO.Path.Combine(System.Environment.CurrentDirectory, "../../../../../Clasharp.Service/bin/Debug/net6.0/Clasharp.Service"));
+#else
+        return Task.FromResult(System.IO.Path.Combine(System.Environment.CurrentDirectory, "Clasharp.WindowsService"));
+#endif
+
+    }
+}
