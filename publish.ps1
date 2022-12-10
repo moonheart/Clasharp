32b0207da40593a543414c293e4c4934f029f756
moonheart <fzp1205561571@hotmail.com>
2022-12-11 01:13:06 +0800

    ci: use windows to avoid issue https://github.com/dotnet/runtime/issues/3828

---
+++ publish.ps1
@@ -0,0 +1,28 @@
+﻿param([string]$configuration)
+
+$ErrorActionPreference = "Stop"
+
+dotnet restore
+dotnet build
+
+mkdir -Force ./release-dir/
+
+function publish([string]$configuration, [string]$rid)
+{
+    if ($configuration -eq "Debug")
+    {
+        dotnet publish Clasharp/Clasharp.csproj -c $configuration -o ./clasharp-$rid/ -r $rid
+        dotnet publish Clasharp.Service/Clasharp.Service.csproj -c $configuration -o ./clasharp-$rid/ -r $rid
+    }
+    else
+    {
+        dotnet publish Clasharp/Clasharp.csproj -c $configuration -o ./clasharp-$rid/ -r $rid -p:PublishSingleFile=true -p:PublishTrimmed=true -p:PublishReadyToRun=true
+        dotnet publish Clasharp.Service/Clasharp.Service.csproj -c $configuration -o ./clasharp-$rid/ -r $rid -p:PublishSingleFile=true -p:PublishTrimmed=true -p:PublishReadyToRun=true
+    }
+    
+    Compress-Archive -Force -Path clasharp-"$rid" -DestinationPath ./release-dir/clasharp-"$rid"-"$configuration".zip
+}
+
+publish -configuration $configuration -rid "win-x64"
+publish -configuration $configuration -rid "linux-x64"
+
