d987b12a5dcc324e38446741f8252ba26caa7246
moonheart <fzp1205561571@hotmail.com>
2022-12-11 00:00:54 +0800

    ci: remove name

--- publish.sh
+++ publish.sh
@@ -9,6 +9,9 @@ function publish() {
   local configuration=$1
   local rid=$2
   local publishArgs=" -c $configuration -o ./clasharp-$rid/ -r $rid -p:PublishSingleFile=true -p:PublishTrimmed=true -p:PublishReadyToRun=true"
+  if [ "$configuration" == "Debug" ]; then
+      publishArgs=" -c $configuration -o ./clasharp-$rid/ -r $rid"
+  fi
   # shellcheck disable=SC2086
   dotnet publish Clasharp/Clasharp.csproj $publishArgs
   # shellcheck disable=SC2086
