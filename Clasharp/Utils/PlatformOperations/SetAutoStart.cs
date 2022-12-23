f48558a9b3bccb54b25cf11d80b9fc171902253d
moonheart <fzp1205561571@hotmail.com>
2022-12-23 21:26:17 +0800

    feat: Auto start for windows

--- Clasharp/Utils/PlatformOperations/SetAutoStart.cs
+++ Clasharp/Utils/PlatformOperations/SetAutoStart.cs
@@ -4,8 +4,10 @@ using System.Threading.Tasks;
 
 namespace Clasharp.Utils.PlatformOperations;
 
-public class SetAutoStart: PlatformSpecificOperation<string, int>
+public class SetAutoStart : PlatformSpecificOperation<string, int>
 {
+    private readonly RunNormalCommand _runNormalCommand = new();
+
     /// <summary>
     /// Set auto start
     /// </summary>
@@ -14,6 +16,15 @@ public class SetAutoStart: PlatformSpecificOperation<string, int>
     public override Task<int> Exec(string path)
     {
         return base.Exec(path);
+    }
+
+    protected override async Task<int> DoForWindows(string path)
+    {
+        var startup = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
+        var link = Path.Combine(startup, "Clasharp.lnk");
+        await _runNormalCommand.Exec(
+            $"powershell \"$s=(New-Object -COM WScript.Shell).CreateShortcut('{link}');$s.TargetPath='{path}';$s.Save()\"");
+        return 0;
     }
 
     protected override async Task<int> DoForLinux(string path)
@@ -27,7 +38,9 @@ Name=Clasharp
 Terminal=false
 Type=Application
 ";
-        await File.WriteAllTextAsync(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".config/autostart/clasharp.desktop"), desktop);
+        await File.WriteAllTextAsync(
+            Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+                ".config/autostart/clasharp.desktop"), desktop);
         return 0;
     }
 }
