5234a3b42733b6398568fe144d6f20502a01269e
moonheart <themoonheart@outlook.com>
2022-12-27 15:19:31 +0800

    fix: hide ExperimentalAcrylicBorder on linux

--- Clasharp/Views/MainWindow.axaml.cs
+++ Clasharp/Views/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
+using System.Runtime.InteropServices;
 using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using Clasharp.ViewModels;
 
@@ -12,6 +14,11 @@ namespace Clasharp.Views
 #if DEBUG
             this.AttachDevTools();
 #endif
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                this.FindControl<ExperimentalAcrylicBorder>("AcrylicBorder1").IsVisible = false;
+                this.FindControl<ExperimentalAcrylicBorder>("AcrylicBorder2").IsVisible = false;
+            }
         }
 
         private void InitializeComponent()
