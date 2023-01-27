cbdae01ef49401030e908af8ea7740d62983b8b6
moonheart <themoonheart@outlook.com>
2023-01-27 11:46:42 +0800

    fix: expander color for light theme

--- Clasharp/DesignTime/DesignProxyGroupListViewModel.cs
+++ Clasharp/DesignTime/DesignProxyGroupListViewModel.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Clasharp.Clash.Models.Proxies;
 using Clasharp.Interfaces;
 using Clasharp.ViewModels;
 
@@ -6,5 +9,17 @@ namespace Clasharp.DesignTime;
 
 public class DesignProxyGroupListViewModel : ViewModelBase, IProxyGroupListViewModel
 {
+    public DesignProxyGroupListViewModel()
+    {
+        ProxyGroupViewModels = new ReadOnlyObservableCollection<ProxyGroupModel>(
+            new ObservableCollection<ProxyGroupModel>(new[]
+            {
+                new ProxyGroupModel(new ProxyGroup()
+                {
+                    Name = "name", All = new List<string>() {"12", "345"}, Type = ProxyGroupType.LoadBalance, Udp = true
+                }, (s, s1) => Task.CompletedTask)
+            }));
+    }
+
     public ReadOnlyObservableCollection<ProxyGroupModel> ProxyGroupViewModels { get; }
 }
