793fc35a7c1ae57b352eb93bfcff15f2f4762836
moonheart <themoonheart@outlook.com>
2023-02-09 11:06:14 +0800

    fix: Make core service path consistent

--- Clasharp/Utils/PlatformOperations/GetCoreServicePath.cs
+++ Clasharp/Utils/PlatformOperations/GetCoreServicePath.cs
@@ -6,20 +6,11 @@ public class GetCoreServicePath: PlatformSpecificOperation<string>
 {
     protected override Task<string> DoForWindows()
     {
-#if DEBUG
-        return Task.FromResult(@"D:\Git_Github\Clasharp\Clasharp.Service\bin\Debug\net6.0\Clasharp.Service.exe");
-#else
         return Task.FromResult(System.IO.Path.Combine(System.Environment.CurrentDirectory, "Clasharp.Service.exe"));
-#endif
     }
 
     protected override Task<string> DoForLinux()
     {
-#if DEBUG
-        return Task.FromResult("/home/moon/Git_Github/Clasharp/Clasharp.Service/bin/Debug/net6.0/Clasharp.Service");
-#else
         return Task.FromResult(System.IO.Path.Combine(System.Environment.CurrentDirectory, "Clasharp.Service"));
-#endif
-
     }
 }
